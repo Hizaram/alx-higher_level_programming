f08c16a2f87234818c82721d85f87c94eca24a89
Hizaram <promisevector@gmail.com>
2022-05-31 21:32:37 +0100

    few corrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,4 @@ if (last_dgt > 5):
 elif (last_dgt == 0):
     print(f"Last digit of {number} is {last_dgt} and is 0")
 elif (last_dgt < 6 and last_dgt != 0):
-    print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{last_dgt} and is less than 6 and not 0")

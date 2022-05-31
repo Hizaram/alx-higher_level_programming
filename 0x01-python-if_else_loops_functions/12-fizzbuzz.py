25826a6393557aa5d5f2d6db42e741bb5440c4f4
Hizaram <promisevector@gmail.com>
2022-05-31 22:15:10 +0100

    Fixed fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,10 +8,7 @@ def fizzbuzz():
         elif (n % 3 == 0):
             print('Fizz', end=' ')
         elif (n % 5 == 0):
-            if (n == 100):
-                print('Buzz', end=' ')
-            else:
-                print('Buzz', end=' ')
+            print('Buzz', end=' ')
         else:
             print(f"{n}", end=' ')
 

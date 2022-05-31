eeb81ad7f7562c3c559f693436bb87729986fb2a
Hizaram <promisevector@gmail.com>
2022-05-31 20:23:54 +0100

    Added the python script that prints hexadecimals

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def get_hexa(number):
+    hex_string = '0123456789abcdef'
+    hex_tens = number // 16
+    hex_units = number % 16
+    if hex_tens > 0:
+        return hex_string[hex_tens] + hex_string[hex_units]
+    else:
+        return hex_string[hex_units]
+
+
+for number in range(99):
+    print(f"{number} = 0x{get_hexa(number)}")

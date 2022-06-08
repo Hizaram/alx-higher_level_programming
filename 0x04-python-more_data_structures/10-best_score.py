e67cc2f6f8a5f122a482d92eb4ccde0aaf433891
Hizaram <promisevector@gmail.com>
2022-06-08 21:22:40 +0100

    Fixed identation

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,21 +3,21 @@
 
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        score = -99999
-        stud = None
+        score = None
+        key = None
+
         for k in a_dictionary.keys():
-            if a_dictionary[k] == "" or a_dictionary[k] is None:
-                return None
-            if a_dictionary[k] > score:
+            if score is None or a_dictionary[k] > score:
                 score = a_dictionary[k]
-                stud = k
-            return stud
+                key = k
+
+        return key
 
 
 if __name__ == '__main__':
     best_score = __import__('10-best_score').best_score
 
-    a_dictionary = {'John': "", 'Bob': "", 'Mike': "", 'Molly': "", 'Adam': ""}
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
     best_key = best_score(a_dictionary)
     print("Best score: {}".format(best_key))
 

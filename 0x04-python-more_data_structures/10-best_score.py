4222cff414ac2ef6763e08ab6237c68c00a943ab
Hizaram <promisevector@gmail.com>
2022-06-08 20:59:19 +0100

    Further corrections[1]

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,9 +6,20 @@ def best_score(a_dictionary):
         score = -99999
         stud = None
         for k in a_dictionary.keys():
-            if a_dictionary[k] is None:
+            if a_dictionary[k] == "" or a_dictionary[k] is None:
                 return None
-            if  a_dictionary[k] > score:
+            if a_dictionary[k] > score:
                 score = a_dictionary[k]
                 stud = k
             return stud
+
+
+if __name__ == '__main__':
+    best_score = __import__('10-best_score').best_score
+
+    a_dictionary = {'John': "", 'Bob': "", 'Mike': "", 'Molly': "", 'Adam': ""}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
+
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))

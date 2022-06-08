e97bbd31fd65b3d1db3747f8f67354a3340b9a18
Hizaram <promisevector@gmail.com>
2022-06-08 20:42:38 +0100

    Corrected the error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
         score = None
         stud = None
         for k in a_dictionary.keys():
-            if score is None or a_dictionary[key] > score:
+            if score is None or a_dictionary[k] > score:
                 score = a_dictionary[k]
                 stud = k
             return stud

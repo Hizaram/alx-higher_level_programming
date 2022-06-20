0a8042ec5e639d6caf81b6a42c5054f4848b72c2
Hizaram <promisevector@gmail.com>
2022-06-20 16:58:49 +0100

    Added the function that prints integers alone and skips all other exceptions in a list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0;
+
+    for b in range(x):
+        try:
+            print("{:d}".format(my_list[b]), end='')
+            count += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+
+    print()
+    return count

c1d4339fbaee108162d085dd576c5c8a1b43a37d
Hizaram <promisevector@gmail.com>
2022-06-22 13:57:05 +0100

    Added the script that defines a class Square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """A Square class"""
+
+    def __init__(self, size):
+        """ the __init__ mehtod for Square class.
+
+        Args:
+            size (int): size of Square.
+
+        """
+        self.__size = size

e5eab09e686ac00ff5878477c39e6101f3fe5d0d
Hizaram <promisevector@gmail.com>
2022-07-06 14:36:36 +0100

    Added the function that returns a Python object from a json file

--- 0x0B-python_input_output/6-load_from_json_file.py
+++ 0x0B-python_input_output/6-load_from_json_file.py
@@ -3,4 +3,14 @@
 import json
 
 
-def load_from_json_file(filename)
+def load_from_json_file(filename):
+    """Creates an object from a json file
+    Args:
+        filename (str): name of the json file
+    Returns:
+        Python Object
+    """
+    Object = None
+    with open(filename, 'r', encoding='utf-8') as f:
+        Object = json.load(f)
+    return Object

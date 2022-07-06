0ba25e4b99f9f4978a9246a5ba9244ea3b4768a2
Hizaram <promisevector@gmail.com>
2022-07-06 20:49:21 +0100

    Fixed some issues

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,9 +12,12 @@ def append_after(filename="", search_string="", new_string=""):
     """
     new_lines = []
     with open(filename, 'r+', encoding='utf-8') as f:
-        for line in f:
+        lines = f.readlines()
+        for line in lines:
             new_lines.append(line)
             if search_string in line:
                 new_lines.append(new_string)
+
+    with open(filename, 'w', encoding='utf-8') as f:
         for line in new_lines:
             f.write(line)

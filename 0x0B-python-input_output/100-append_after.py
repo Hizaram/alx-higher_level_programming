8847ef2a87080ceb0c37c0a5f8690977d14f7aea
Hizaram <promisevector@gmail.com>
2022-07-06 20:12:31 +0100

    Added the function that appends a new string after the target line of a file(1)

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -20,4 +20,4 @@ def append_after(filename="", search_string="", new_string=""):
 
     with open(filename, 'w', encoding='utf-8') as nf:
         for line in new_lines:
-            f.write(line)
+            nf.write(line)

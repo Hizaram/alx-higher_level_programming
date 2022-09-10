00a07d5f00f60a13077ce226d2ed4e19f6cf2e8b
Hizaram <promisevector@gmail.com>
2022-09-10 20:42:26 +0100

    Fixed pycodestyle standard errors

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     c = db.cursor()
     c.execute("SELECT * FROM `cities` AS `c` INNER JOIN `states` AS `s` \
               ON `c`.`state_id` = `s`.`id` ORDER BY `c`.`id`")
-    print(", ".join([city[2] for city in c.fetchall() if city[4] == sys.argv[4]]))
+    print(", ".join([city[2] for city in c.fetchall()
+          if city[4] == sys.argv[4]]))

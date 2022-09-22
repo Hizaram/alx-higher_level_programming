47ee7e34acf65fdee0ebeae48fc6558bb5d5a159
Hizaram <promisevector@gmail.com>
2022-09-22 19:40:51 +0100

    Added the script that sends a request to the URL provided as argument and returns the size of the body response[1]

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Gets the size of the size of the body of the response after a request is sent to a URL
 curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2

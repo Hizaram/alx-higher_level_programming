9c61cea77c0e67f152f3895feecf36b9f94ac78e
Hizaram <promisevector@gmail.com>
2022-09-22 20:27:20 +0100

    Added the script that displays the body of the response gotten from a GET request sent to a URL[2]

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a GET request to a URL and displays the body of the response
-curl -H 'X-School-User-Id: 98' -sLX GET "$1"
+curl -H 'X-School-User-Id:98' -sLX GET "$1"

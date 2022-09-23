69c4638b6c6378f9c853974dc1e5e0e49af277d1
Hizaram <promisevector@gmail.com>
2022-09-23 21:03:47 +0100

    Added the script that sends a POST request to a URL argument with email as parameter and returns the utf-8 decoded response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that sends a POST request to a URL argument with email
+as parameter and displays the utf-8 encoded response
+"""
+import sys
+from urllib import request
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        form_data = bytes(parse.urlencode([('email', email)]), 'utf-8')
+        with request.urlopen(sys.arv[1], data=form_data) as response:
+            print(response.read().decode('utf-8'))

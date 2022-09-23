774ad21ab90b90f054a93fffe2d70e8f068c25db
Hizaram <promisevector@gmail.com>
2022-09-23 23:59:29 +0100

    Added the script that displays the body of the response or the erro code of a requested URL

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that requests a URL and returns the body of the response
+or the error code
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        response = requests.get(url)
+        if response.status_code >= 400:
+            print("Error code: {}".format(reponse.status_code))
+        else:
+            print(response.text)

35f71cf461c627983f79f6fc353be2450a179e4e
Hizaram <promisevector@gmail.com>
2022-09-24 00:22:49 +0100

    Fixed the 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,9 +6,13 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    query = sys.argv[1] if len(sys.argv) > 1 else ""
-    # if len(query) > 0 and not query[0].isalpha():
-    #     query = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+        query = q
+    else:
+        q = ""
+        query = q
+
     form_data = [('q', query)]
     response = requests.post(url, data=form_data)
     try:

7b60e0ee416f21259442f89bb37802c6c8855cd9
Hizaram <promisevector@gmail.com>
2022-09-24 00:30:18 +0100

    Fixed the 8-json_api.py[1]

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,20 +6,15 @@ import requests
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-        query = q
-    else:
-        q = ""
-        query = q
+    if len(sys.argv) > 1 ? q = sys.argv[1] : q = ""
 
-    form_data = [('q', query)]
-    response = requests.post(url, data=form_data)
-    try:
-        json_content = response.json()
-        if json_content:
-            print('[{}] {}'.format(json_content['id'], json_content['name']))
-        else:
-            print('No result')
-    except Exception:
-        print('Not a valid JSON')
+        form_data = [('q', q)]
+        response = requests.post(url, data=form_data)
+        try:
+            json_content = response.json()
+            if json_content:
+                print('[{}] {}'.format(json_content['id'], json_content['name']))
+            else:
+                print('No result')
+        except Exception:
+            print('Not a valid JSON')

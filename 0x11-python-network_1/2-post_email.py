efd341744c2eb15df09f34649cf4a093ef0dd9ef
Hizaram <promisevector@gmail.com>
2022-09-26 19:42:38 +0100

    Fixed the import statement[1]

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         url = sys.argv[1]
         email = sys.argv[2]
         form_data = bytes(parse.urlencode([('email', email)]), 'utf-8')
-        with request.urlopen(sys.arv[1], data=form_data) as response:
+        with request.urlopen(sys.argv[1], data=form_data) as response:
             print(response.read().decode('utf-8'))

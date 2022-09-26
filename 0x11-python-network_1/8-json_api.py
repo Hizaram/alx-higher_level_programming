f81e5620311290eaf343d7c57c05f6742d9113cc
Hizaram <promisevector@gmail.com>
2022-09-26 19:22:56 +0100

    Fixed the mode

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,13 +7,13 @@ import requests
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) > 1 ? q = sys.argv[1] : q = ""
-
         form_data = [('q', q)]
         response = requests.post(url, data=form_data)
         try:
             json_content = response.json()
             if json_content:
-                print('[{}] {}'.format(json_content['id'], json_content['name']))
+                print('[{}] {}'.format(json_content['id'], json_content
+                      ['name']))
             else:
                 print('No result')
         except Exception:
